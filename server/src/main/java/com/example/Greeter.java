e5a1d23e545b1126f7f41888fc07f57d4e196d80
root <root@code1-centos.code1.local>
2018-06-02 21:39:40 -0700

    sdfsfd sdfsaf

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -14,9 +14,10 @@ public class Greeter {
   }
 
  /**
-  * @param blah blah blah
-  * @return foo foo foo
+  * @param someone the name of a person
+  * @return greeting string
   */
+
   public final String greet(final String someone) {
     return String.format("Hello stinky, %s!", someone);
   }
